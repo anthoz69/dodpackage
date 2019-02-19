832a27f5775def16a74bd8e2731994e3570deb2a
Anthoz69 <st.anthoz69@gmail.com>
2019-02-19 14:34:35 +0700

    change file name to unique not duplicate

--- src/dodStorage.php
+++ src/dodStorage.php
@@ -3,18 +3,28 @@
 namespace anthoz69\dodpackage;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Image;
 
 class dodStorage {
     private $resultFile;
     private $resultFilePath;
 
+    public function uniqueFilename($file, $storeFolder) {
+        do {
+            $uniqueFilename = uniqid(Str::random(8)) . '.' . $file->getClientOriginalExtension();
+            $fullPathStore = $storeFolder . DIRECTORY_SEPARATOR . $uniqueFilename;
+            $exists = Storage::disk('public')->exists($fullPathStore);
+        } while ($exists);
+        return $fullPathStore;
+    }
+
     public function delete($storePath, $replace = '/storage') {
         return Storage::disk('public')->delete(str_replace($replace, '', $storePath));
     }
 
     public function store($file, $storeFolder) {
-        $storePath = $file->hashName($storeFolder);
+        $storePath = $this->uniqueFilename($file, $storeFolder);
         Storage::disk('public')->put($storePath, file_get_contents($file));
         return Storage::url($storePath, 'public');
     }
@@ -25,21 +35,21 @@ class dodStorage {
     }
 
     public function cropImage($file, $storeFolder, $width = 500, $height = 500) {
-        $this->resultFilePath = $file->hashName($storeFolder);
+        $this->resultFilePath = $this->uniqueFilename($file, $storeFolder);
         $img = Image::make($file);
         $this->resultFile = $img->fit($width, $height);
         return $this->save();
     }
 
     public function resizeImage($file, $storeFolder, $width = 500, $height = 500) {
-        $this->resultFilePath = $file->hashName($storeFolder);
+        $this->resultFilePath = $this->uniqueFilename($file, $storeFolder);
         $img = Image::make($file);
         $this->resultFile = $img->resize($width, $height);
         return $this->save();
     }
 
     public function cropImageRatio($file, $storeFolder, $width = 500, $height = 500) {
-        $this->resultFilePath = $file->hashName($storeFolder);
+        $this->resultFilePath = $this->uniqueFilename($file, $storeFolder);
         $img = Image::make($file);
         $this->resultFile = $img->fit($width, $height, function ($constraint) {
             $constraint->aspectRatio();
@@ -49,7 +59,7 @@ class dodStorage {
     }
 
     public function resizeImageRatio($file, $storeFolder, $width = 500, $height = 500) {
-        $this->resultFilePath = $file->hashName($storeFolder);
+        $this->resultFilePath = $this->uniqueFilename($file, $storeFolder);
         $img = Image::make($file);
         $this->resultFile = $img->resize($width, $height, function ($constraint) {
             $constraint->aspectRatio();
