01531e793d99a1ee295ae7b15ade24aa3279d28a
anthoz69 <st.anthoz69@gmail.com>
2019-08-07 15:55:50 +0700

    update currency function

--- src/Helpers/currency.php
+++ src/Helpers/currency.php
@@ -2,9 +2,5 @@
 
 function setCurrency($number, $percision = 2)
 {
-    if ($percision === 0) {
-        return number_format($number, 0, '.', ',');
-    }
-
-    return number_format($number, $percision, '.', ',');
+    return number_format(floor($number * 100) / 100, $percision, '.', ',');
 }
